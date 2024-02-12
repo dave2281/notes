b0c6c4588f4d0cb41cad1d0ff70a00e247d678e0
dave2281 <dava.bigboy@gmail.com>
2024-02-12 16:46:04 +0200

    added some features: devise, new notes scaffold, homepage+navbar

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,4 +5,6 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "bootstrap", to: "bootstrap.min.js"
+pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" # @5.3.2
+pin "@rails/ujs", to: "@rails--ujs.js" # @7.1.3
+
