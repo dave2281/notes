998271095750c15a50dfc3356f57e5bb73ca2cfd
dave2281 <dava.bigboy@gmail.com>
2024-04-17 18:24:41 +0300

    fix sign_up reCAPTCHA func

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,12 +1,17 @@
 class RegistrationsController < Devise::RegistrationsController
   def create
     build_resource(sign_up_params)
-    if verify_recaptcha(model: self.resource) && resource.save
-      sign_up(resource_name, resource)
-      redirect_to home_index_path
+    if verify_recaptcha(model: resource)
+      if resource.save
+        sign_up(resource_name, resource)
+        redirect_to home_index_path
+      else
+        clean_up_passwords(resource)
+        redirect_to new_user_registration_path, alert: resource.errors.full_messages.join(", ")
+      end
     else
       clean_up_passwords(resource)
-      render :new, alert: "reCAPTCHA verification failed."
+      render :new, alert: "reCAPTCHA false"
     end
   end
 end
