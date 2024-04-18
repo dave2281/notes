13e553f0340be8b4aff3b9fd770e6e3808d16dcf
dave2281 <dava.bigboy@gmail.com>
2024-04-18 11:49:48 +0300

    fixed sign up recaptcha func

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,14 +4,21 @@ class RegistrationsController < Devise::RegistrationsController
     if verify_recaptcha(model: resource)
       if resource.save
         sign_up(resource_name, resource)
-        redirect_to home_index_path
+        redirect_to after_sign_up_path_for(resource)
       else
-        clean_up_passwords(resource)
-        redirect_to new_user_registration_path, alert: resource.errors.full_messages.join(", ")
+        clean_up_passwords resource
+        set_minimum_password_length
       end
     else
       clean_up_passwords(resource)
-      render :new, alert: "reCAPTCHA false"
+      flash[:alert] = "Failed to pass reCAPTCHA. Please try again."
+      redirect_to new_user_registration_path
     end
+  end
+
+  private
+
+  def after_sign_up_path_for(resource)
+    home_index_path
   end
 end
