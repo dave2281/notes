8b8192fc24fcc8ad06f72fba8dd0af8d8fc73293
dave2281 <dava.bigboy@gmail.com>
2024-05-07 16:42:35 +0300

    added some changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,6 +98,6 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :mailgun
   config.action_mailer.mailgun_settings = {
     api_key: ENV['MAILGUN_API_KEY'],
-    domain: "notes-6t3l.onrender.com"
+    domain: ENV['MAILGUN_DOMAIN']
   }
 end
